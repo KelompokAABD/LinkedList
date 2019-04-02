eef0f7d1ad9e3cae6517fa16f88f70d53af0d740
ArafiaIsnayuAkaf <48676201+ArafiaIsnayuAkaf@users.noreply.github.com>
2019-04-02 20:05:13 +0700

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,24 +1,13 @@
-#include <iostream>
-using namespace std;
-
-struct node{
-    int data;
-    node *next;
-};
-node *head, *tail;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
-}
+int main (){
+    cout<<"LINKED LIST\n\n";
+    cout<<"==================";
+    cout<<"1. Insert Depan\n";
+    cout<<"2. Insert Belakang\n";
+    cout<<"3. Hapus Depan\n";
+    cout<<"4. Hapus Belakang\n";
+    cout<<"5. Insert Sesuai letak\n";
+    cout<<"6. Keluar Program\n\n";
+    menu:
+    int pil;
+    int nilai, letak;
+    cout<<"Masukkan Pilihan = ";cin>>pil;
