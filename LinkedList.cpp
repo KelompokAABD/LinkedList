30039edbcd79d6e2704923b6ad4dc56407b3832b
MuhammadNurAshiddiqi <48676131+MuhammadNurAshiddiqi@users.noreply.github.com>
2019-04-03 18:46:48 +0700

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
+// #include <windows.h> ini buat pake windows
 using namespace std;
 
 struct node{
     int data;
     node *next;
-           };
+};
 node *head, *tail;
 
 int isEmpty(){
     if (head == NULL)
         return 1;
     else return 0;
-             }
+}
+
+
 void InsFront(int nilai){
     node *baru;
-    beru = new node;
+    baru = new node;
     baru->data=nilai;
     baru->next=NULL;
-}else{
-    baru->next=head;
-    head=baru;
-}
-cout<<"\n data "<<nilai<<" dimasukan ke dalam list\n";
+    if(isEmpty()==1){
+        head = baru;
+        head->next=NULL;
+    }else{
+        baru->next=head;
+        head=baru;
+    }
+    cout<<"\n Data "<<nilai<<" dimasukkan ke dalam list \n";
 }
 
 void InsBack(int nilai){
@@ -29,10 +35,70 @@ void InsBack(int nilai){
     baru=new node;
     baru->data=nilai;
     baru->next=NULL;
-}else{
-    bantu=head;
-    while(bantu->next!=NULL){
-        bantu=bantu->next;
+    if(isEmpty()==1){
+        head=baru;
+        head->next=NULL;
+    }else{
+        bantu=head;
+        while(bantu->next!=NULL){
+            bantu=bantu->next;
+        }
+        bantu->next=baru;
+    }
+    cout<<"\n Data "<<nilai<<" dimasukkan ke dalam list \n";
+}
+
+void Display(){
+    node *baru=new node;
+    baru=head;
+    if (isEmpty()==0){
+        cout<<"\n Data yang ada dalam list \n\n";
+    while(baru!=NULL){
+        cout<<baru->data<<" ";
+        baru=baru->next;
+        }
+    }
+    cout<<endl<<endl;
+}
+
+void DelFront()
+  {
+    node *baru=new node;
+    baru=head;
+    head=head->next;
+    delete baru;
+  }
+
+void DelBack(){
+    node *after=new node;
+    node *before=new node;
+    after=head;
+    while(after->next!=NULL)
+    {
+      before=after;
+      after=after->next;
+    }
+    tail=before;
+    before->next=NULL;
+    delete after;
+}
+
+void InsertPos(int letak, int nilai){
+    node *before=new node;
+    node *after=new node;
+    node *baru=new node;
+    after=head;
+    for(int i=1;i<letak;i++)
+    {
+      before=after;
+      after=after->next;
+    }
+    baru->data=nilai;
+    before->next=baru;
+    baru->next=after;
+
+    cout<<"\n\n Data "<<nilai<<" dimasukkan ke dalam list \n";
+}
     
 
 int main (){
@@ -48,7 +114,7 @@ int main (){
     int pil;
     int nilai, letak;
     cout<<"Masukkan Pilihan = ";cin>>pil;
-switch (pil){
+    switch (pil){
         case 1 : cout<<"Masukkan data = ";
                  cin>>nilai;
                  InsFront(nilai);
@@ -74,3 +140,6 @@ switch (pil){
                  goto menu;
       case 6 : cout<<"\n\nTERIMA KASIH :) \n";
                  exit(0);
+            }
+    return 0;
+}
